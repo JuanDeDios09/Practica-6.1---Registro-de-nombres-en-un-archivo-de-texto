956a25ad695c921cf7a285471606bfa81403385d
JuanDeDios09 <252310070@itslerdo.edu.mx>
2026-05-30 12:28:52 -0600

    Se corrigio el index.php

---
+++ Codigo/registro.php
@@ -0,0 +1,34 @@
+<?php
+// areglo de nombres de asistentes 
+// Este script crea un archivo de texto y escribe los nombres de los asistentes en el
+
+// Nombre del archivo donde se guradaran los nombres 
+$nombreArchivo = "Asistentes.txt";
+
+try {
+  
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        try {
+            $nombre = trim($_POST['nombre']);
+
+            if (empty($nombre)) {
+                throw new Exception("Debe ingresar un nombre");
+            }
+            
+            $archivo = fopen(Asistentes.txt "a");
+
+            if (!$archivo) {
+                throw new Exception("No se pudo abrir el archivo.");
+            }
+
+            fwrite($archivo, $nombre . PHP_EOL);
+
+            fclose($archivo);
+
+            $resultado = "Nombre guardado correctamente";
+        }
+
+    }catch (Exception $e) {
+        echo "Ocurrio un error: " . $e->getMessage();
+    }// catch
+?>
